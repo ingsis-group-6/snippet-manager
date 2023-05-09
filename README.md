a5a1d03f5db3db47af982c77345e8575f9295337
Numa Leone <numa.leone>
2023-05-08 22:20:53 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # snippet-manager
+
+This is the API to get, create, edit and save snippets.
+
+## List of endpoints
+* All snippets: http://localhost:8080/snippets
+* Snippet by Id: http://localhost:8080/snippets/{snippet-id}
+* Snippets by user Id: http://localhost:8080/snippets/{user-id}
+* Edit snippet: http://localhost:8080/snippets/{id}/edit
+* Save snippet: http://localhost:8080/snippets/{id}/save
--- src/main/kotlin/ingsis/snippetmanager/SnippetManagerApplication.kt
+++ src/main/kotlin/ingsis/snippetmanager/SnippetManagerApplication.kt
@@ -1,11 +1,38 @@
 package ingsis.snippetmanager
 
+import ingsis.snippetmanager.snippet.Snippet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @SpringBootApplication
-class SnippetManagerApplication
+@RestController
+class SnippetManagerApplication{
 
+	@GetMapping("/")
+	fun url(): String {
+		return "Go to /hello or /snippets";
+	}
+	@GetMapping("/hello")
+	fun hello(): List<String> {
+		return listOf("Hello", " World, ", "this is Numa");
+	}
+
+	@GetMapping("/snippets")
+	fun snippets():List<Snippet>{
+		return listOf(
+			Snippet(1, "Contenido 1",  LocalDate.now()),
+			Snippet(2, "Contenido 2",  LocalDate.now()),
+			Snippet(3, "Contenido 3",  LocalDate.now()),
+			Snippet(4, "Contenido 4",  LocalDate.now()),
+		);
+	}
+}
 fun main(args: Array<String>) {
 	runApplication<SnippetManagerApplication>(*args)
 }
+
+
+
